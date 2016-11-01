4fb029a03ffb7ae1bed8c55effbb05994e2f55f3
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-01 13:45:41 +0000

    gradle init

---
+++ README.md
@@ -0,0 +1,1 @@
+https://gist.github.com/alexlafroscia/c6757de349b27e34eff6#file-junit-gradle-how-to-md
---
+++ src/test/java/BowlingTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class BowlingGameTest extends TestCase {
+  public void testGutterGame() throws Exception {
+    Game g = new Game();
+  }
+}
