9b65f8405c8641582b5970351b007eb4d3b94767
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-02 09:16:46 +0000

    first test passing

--- src/test/java/BowlingTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-import org.junit.*;
-
-public class BowlingGameTest extends TestCase {
-  public void testGutterGame() throws Exception {
-    Game g = new Game();
-  }
-}
