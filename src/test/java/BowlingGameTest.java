2b88a731df336203ff476c39df321153812a460e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-02 09:35:57 +0000

    second test passing refactored tests

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -2,10 +2,30 @@ import junit.framework.TestCase;
 
 
 public class BowlingGameTest extends TestCase {
-public void testGutterGame() throws Exception {
+  private Game g;
+
+  protected void setUp() throws Exception {
+    g = new Game();
+  }
+
+
+  public void testGutterGame() throws Exception {
     Game g = new Game();
     for(int i =0; i <20; i++)
-    g.roll(0);
+      g.roll(0);
     assertEquals(0, g.score());
-}
+  }
+
+  public void testAllOnes() throws Exception {
+    Game g = new Game();
+    for (int i = 0; i < 20; i ++)
+      g.roll(1);
+    assertEquals(20, g.score());
+
+
+  }
+
+
+
+
 }
